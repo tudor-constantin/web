e1c56d04e9802e38f9e3639e1837fd398916ba7b
Tudor <41721145+tudor-constantin@users.noreply.github.com>
2022-04-17 18:18:32 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // NAVBAR
+    $('.navbar-nav .nav-link').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 50) {
+            $(".navbar").addClass("sticky-nav");
+        } else {
+            $(".navbar").removeClass("sticky-nav");
+        }
+    });
+
+    // BACKSTRETCH SLIDESHOW
+    $('#section_1').backstretch([
+      "images/slide/treball1.jpg", 
+      "images/slide/treball2.jpg",
+      "images/slide/treball3.jpg"
+    ],  {duration: 5000, fade: 750});
+    
+  })(window.jQuery);
+
+
